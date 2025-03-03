7287c6ffc62bd971a9db92c1f625811a0e40f64f
BigyCenz <60927355+BigyCenz@users.noreply.github.com>
2025-03-03 18:33:39 +0100

    Update ArtNet_ESP.ino

--- ArtNet_ESP.ino
+++ ArtNet_ESP.ino
@@ -2,6 +2,8 @@
 #include <WebServer.h>
 #include <Preferences.h>
 #include <WiFiUdp.h>
+#include "ThingSpeak.h"
+#include <HTTPClient.h>
 //#include "thingProperties.h"
 
 
@@ -32,11 +34,19 @@ uint16_t opCode;
 uint16_t universe;
 uint16_t length;
 
+unsigned long int channel_ID = 2862162;
+const char* ApiKeyR = "9KLK4ID4TIJZJQEL";
+const char* serverThingSpeak = "http://api.thingspeak.com/update";
+const int fiel_num = 1;
+WiFiClient thingSpeakClient;
+unsigned long previousMillis = 0;
+const long interval = 17000;
+
 // Inizializzazione
 void setup() {
 
   Serial.begin(115200);
-  Serial2.begin(250000,SERIAL_8N1,16,17);
+  Serial2.begin(250000, SERIAL_8N1, 16, 17);
 
   preferences.begin("dmx_settings", false);
   loadSettings();
@@ -50,9 +60,13 @@ void setup() {
   // Inizializza la connessione al cloud
   //initProperties();
   //ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  Serial.println("Starting thuingspeak...");
+  ThingSpeak.begin(thingSpeakClient);
+  //int risposta = ThingSpeak.writeField(channel_ID, fiel_num, 15, ApiKeyR);
+  //Serial.println(risposta);
+
 
   Serial.println("Setup completato.");
-
 }
 
 // Esecuzione principale
@@ -62,7 +76,7 @@ void loop() {
   handleArtNetPacket();
 }
 
-void onArtnetChange()  {
+void onArtnetChange() {
   // Add your code here to act upon Artnet change
   Serial.println("OK");
 }
@@ -76,7 +90,7 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t* data) {
 
 // Funzione - Gestisce la ricezione di pacchetti Art-Net
 void handleArtNetPacket() {
-  
+
   packetSize = udp.parsePacket();
 
   if (packetSize > 0 && packetSize <= MAX_BUFFER_LENGTH_ARTNET) {
@@ -101,8 +115,8 @@ void handleArtNetPacket() {
     // Versione protocollo
     //uint16_t protocolVersion = (packetBuffer[10] << 8) | packetBuffer[11];
     //if (protocolVersion < 14) {  // Versione minima accettata (0x0E)
-      //Serial.println("Versione protocollo non supportata!");
-      //return;
+    //Serial.println("Versione protocollo non supportata!");
+    //return;
     //}
     //Serial.println("Versione protocollo OK!");
 
@@ -115,15 +129,15 @@ void handleArtNetPacket() {
     //Serial.println(physical);
 
     // Universo DMX
-    universe = packetBuffer[14] | (packetBuffer[15] << 8); 
+    universe = packetBuffer[14] | (packetBuffer[15] << 8);
     //Serial.print("Universe: ");
     //Serial.println(universe);
 
     // Lunghezza dei dati DMX
     length = packetBuffer[17] | (packetBuffer[16] << 8);
     //if (length > 512) {
-      //Serial.println("Errore: lunghezza DMX non valida!");
-      //return;
+    //Serial.println("Errore: lunghezza DMX non valida!");
+    //return;
     //}
     //Serial.print("Lunghezza dati DMX: ");
     //Serial.println(length);
@@ -131,73 +145,84 @@ void handleArtNetPacket() {
     // Inizio dati DMX
     uint8_t* data = &packetBuffer[18];
 
+    //send info to server
+    unsigned long currentMillis = millis();
+
+
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      int res = ThingSpeak.writeField(channel_ID, fiel_num, data[0], ApiKeyR);
+      if (res==200) Serial.println("INVIATI!!"); else Serial.print("ERRORE");
+    }
+    
+    //delay(1500);
+
     // Funzione per gestire i dati DMX e inviarli al ricevitore
     onDmxFrame(universe, length < MAX_CHANNEL_LIMIT ? length : MAX_CHANNEL_LIMIT, data);
-
   }
 }
 
 // Decide se connettersi a un WiFi esistente oppure creare il suo
 void setupWiFi() {
 
-    if (useApMode) {
-        WiFi.softAP(ssidAP.c_str(), passwordAP.c_str());
-        IP = WiFi.softAPIP();
-        Serial.println("Rete generata! IP: " + IP.toString());
-    } else {
-        WiFi.begin(ssidSta.c_str(), passwordSta.c_str());
-        Serial.println("Connessione al WiFi...");
-        while (WiFi.status() != WL_CONNECTED) {
-            delay(1000);
-            Serial.print(".");
-        }
-        Serial.println("Connesso! IP: " + WiFi.localIP().toString());
+  if (useApMode) {
+    WiFi.softAP(ssidAP.c_str(), passwordAP.c_str());
+    IP = WiFi.softAPIP();
+    Serial.println("Rete generata! IP: " + IP.toString());
+  } else {
+    WiFi.begin(ssidSta.c_str(), passwordSta.c_str());
+    Serial.println("Connessione al WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print(".");
     }
+    Serial.println("Connesso! IP: " + WiFi.localIP().toString());
+  }
 }
 
 // Gestisce la pagina web di configurazione
 void setupWebPage() {
-    server.on("/", HTTP_GET, []() {
-        String page = "<html><body><h1>ESP32 DMX Controller</h1>";
-        page += "<form method='POST' action='/save'>";
-        page += "<h3>AP Mode</h3>SSID: <input type='text' name='ssid_ap' value='" + ssidAP + "'><br>";
-        page += "Password: <input type='text' name='password_ap' value='" + passwordAP + "'><br><br>";
-        page += "<h3>Station Mode</h3>SSID: <input type='text' name='ssid_sta' value='" + ssidSta + "'><br>";
-        page += "Password: <input type='text' name='password_sta' value='" + passwordSta + "'><br><br>";
-        page += "<input type='radio' name='mode' value='ap' " + String(useApMode ? "checked" : "") + "> AP Mode<br>";
-        page += "<input type='radio' name='mode' value='sta' " + String(!useApMode ? "checked" : "") + "> Station Mode<br><br>";
-        page += "<input type='submit' value='Save'>";
-        page += "</form></body></html>";
-        server.send(200, "text/html", page);
-    });
+  server.on("/", HTTP_GET, []() {
+    String page = "<html><body><h1>ESP32 DMX Controller</h1>";
+    page += "<form method='POST' action='/save'>";
+    page += "<h3>AP Mode</h3>SSID: <input type='text' name='ssid_ap' value='" + ssidAP + "'><br>";
+    page += "Password: <input type='text' name='password_ap' value='" + passwordAP + "'><br><br>";
+    page += "<h3>Station Mode</h3>SSID: <input type='text' name='ssid_sta' value='" + ssidSta + "'><br>";
+    page += "Password: <input type='text' name='password_sta' value='" + passwordSta + "'><br><br>";
+    page += "<input type='radio' name='mode' value='ap' " + String(useApMode ? "checked" : "") + "> AP Mode<br>";
+    page += "<input type='radio' name='mode' value='sta' " + String(!useApMode ? "checked" : "") + "> Station Mode<br><br>";
+    page += "<input type='submit' value='Save'>";
+    page += "</form></body></html>";
+    server.send(200, "text/html", page);
+  });
 
-    server.on("/save", HTTP_POST, []() {
-        ssidAP = server.arg("ssid_ap");
-        passwordAP = server.arg("password_ap");
-        ssidSta = server.arg("ssid_sta");
-        passwordSta = server.arg("password_sta");
-        useApMode = (server.arg("mode") == "ap");
-        saveSettings();
-        server.send(200, "text/html", "<html><body><h1>Settings Saved</h1><a href='/'>Back</a></body></html>");
-        delay(1000);
-        ESP.restart();
-    });
+  server.on("/save", HTTP_POST, []() {
+    ssidAP = server.arg("ssid_ap");
+    passwordAP = server.arg("password_ap");
+    ssidSta = server.arg("ssid_sta");
+    passwordSta = server.arg("password_sta");
+    useApMode = (server.arg("mode") == "ap");
+    saveSettings();
+    server.send(200, "text/html", "<html><body><h1>Settings Saved</h1><a href='/'>Back</a></body></html>");
+    delay(1000);
+    ESP.restart();
+  });
 }
 
 // Salva le impostazioni su memoria non volatile
 void saveSettings() {
-    preferences.putString("ssidAP", ssidAP);
-    preferences.putString("passwordAP", passwordAP);
-    preferences.putString("ssidSta", ssidSta);
-    preferences.putString("passwordSta", passwordSta);
-    preferences.putBool("useApMode", useApMode);
+  preferences.putString("ssidAP", ssidAP);
+  preferences.putString("passwordAP", passwordAP);
+  preferences.putString("ssidSta", ssidSta);
+  preferences.putString("passwordSta", passwordSta);
+  preferences.putBool("useApMode", useApMode);
 }
 
 // Carica le impostazione da memoria non volatile
 void loadSettings() {
-    ssidAP = preferences.getString("ssidAP", ssidAP);
-    passwordAP = preferences.getString("passwordAP", passwordAP);
-    ssidSta = preferences.getString("ssidSta", "");
-    passwordSta = preferences.getString("passwordSta", "");
-    useApMode = preferences.getBool("useApMode", true);
+  ssidAP = preferences.getString("ssidAP", ssidAP);
+  passwordAP = preferences.getString("passwordAP", passwordAP);
+  ssidSta = preferences.getString("ssidSta", "");
+  passwordSta = preferences.getString("passwordSta", "");
+  useApMode = preferences.getBool("useApMode", true);
 }
